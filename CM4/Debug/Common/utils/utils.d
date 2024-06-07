23b207d4777444ddf79d3d9ba7370f054acfcc44
gian793 <giancarlo.marcolin@gmail.com>
2024-06-07 05:57:15 +0200

    deleted files

--- CM4/Debug/Common/utils/utils.d
+++
@@ -1,4 +0,0 @@
-Common/utils/utils.o: \
- /home/giancarlo/Documents/g793Projects/stm32h755mx/Common/utils/utils.cpp \
- /home/giancarlo/Documents/g793Projects/stm32h755mx/Common/utils/utils.h
-/home/giancarlo/Documents/g793Projects/stm32h755mx/Common/utils/utils.h:
